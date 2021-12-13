1b4568832820aaa4d9cca615741bdd3a712ecf7b
rtracy14 <89800889+rtracy14@users.noreply.github.com>
2021-12-13 12:07:07 -0600

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace CS_ASP_024
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
